ef9d5e27a55d64efdf04a0901a1e6965e92a73c0
Karolina Hyla <karatakarabe@gmail.com>
2023-05-16 15:59:54 +0200

    Add Column component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'font-awesome/css/font-awesome.min.css';
 import './styles/normalize.scss';
 import './styles/global.scss';
+
 
 ReactDOM.render(
   <App />,
