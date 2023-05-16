e0f63041355798b406fb11bc5a5f491abb6696b0
Karolina Hyla <karatakarabe@gmail.com>
2023-05-16 14:56:46 +0200

    Add Button and TextInput code

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,6 +5,7 @@ const List = () =>{
 		<div className={styles.list}>
 			<header className={styles.header}>
 				<h2 className={styles.title}>Things to do <span>soon</span></h2>
+			</header>
 				<p className={styles.description}>Interesting things I want to check out</p>
 				<section className={styles.columns}>
   				<article>
@@ -16,10 +17,9 @@ const List = () =>{
 					<article>
 						<h2 className="title">Games</h2>
 					</article>
-				</section>	
-			</header>
+				</section>			
 		</div>
-	)
+	);
 };
 
 export default List;
