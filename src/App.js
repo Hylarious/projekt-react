7cd2007fe78d8325873de2856fba60026c89f98e
Karolina Hyla <karatakarabe@gmail.com>
2023-06-06 19:22:01 +0200

    Add ReactRouter and WrongAdress component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Container from './components/Container/Container.js';
 import NavBar from './components/NavBar/NavBar.js';
 import Home from './components/Home/Home.js';
+import About from './components/About/About.js'
+import Favorite from './components/Favorite/Favorite.js'
+import { Routes, Route} from 'react-router-dom'
+import WrongAddress from './components/WrongAddress/WrongAddress.js';
 
 
 const App = () => {
@@ -8,7 +12,12 @@ const App = () => {
     <div>
       <NavBar />
       <Container>
-        <Home />
+        <Routes>
+          <Route  path="/" element={<Home />} />
+          <Route  path="/about" element={<About />} />
+          <Route  path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<WrongAddress />}/>
+        </Routes>
       </Container>
     </div>
   );
---
+++ src/components/WrongAddress/WrongAddress.js
@@ -0,0 +1,9 @@
+import PageTitle from "../PageTitle/PageTitle"
+
+const WrongAddress = () => {
+	return (
+		<PageTitle>404 not found</PageTitle>
+	)
+}
+
+export default WrongAddress
