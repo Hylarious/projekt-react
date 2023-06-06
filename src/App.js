c467ec78ff027bde04f6e226d163afcbafd79780
Karolina Hyla <karatakarabe@gmail.com>
2023-06-06 18:53:38 +0200

    Add home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Container from './components/Container/Container.js';
-import Hero from './components/Hero/Hero.js'
-import SearchForm from './components/SearchForm/SearchForm.js'
-import List from './components/List/List.js'
 import NavBar from './components/NavBar/NavBar.js';
+import Home from './components/Home/Home.js';
 
 
 const App = () => {
@@ -10,9 +8,7 @@ const App = () => {
     <div>
       <NavBar />
       <Container>
-        <Hero />
-        <SearchForm />
-        <List />
+        <Home />
       </Container>
     </div>
   );
